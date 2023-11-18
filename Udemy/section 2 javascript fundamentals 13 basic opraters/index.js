4986e4bee10d9e9d52222b03f8c6fb24a7bc8b74
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-18 15:32:54 +0530

    operators

---
+++ Udemy/section 2 javascript fundamentals 13 basic opraters/index.js
@@ -0,0 +1,23 @@
+const yogesh = 2027 - 1984;
+
+console.log(yogesh);
+// Assignment Operator is =
+const x = 2;
+const y = 2;
+const z = 4;
+let total = x + y;
+console.log((total = z));
+console.log(`The Assignment Operator (=) assigns a value to a variable:`);
+console.log(`The Addition Operator (+) adds numbers:`);
+console.log(`The Multiplication Operator (*) multiplies numbers:`);
+console.log(
+  `Arithmetic Operators are used to perform arithmetic on numbers: +,-,/,*,**,%,++,--,`
+);
+console.log(
+  `The Addition Assignment Operator (+=) adds a value to a variable. =, +=, -=, *=, /=, %=, **=`
+);
+console.log(
+  `JavaScript Comparison Operators: == , === , != , !== , > , < , >= , <= , ?`
+);
+
+console.log(`JavaScript Logical Operators : && , || , !`);
