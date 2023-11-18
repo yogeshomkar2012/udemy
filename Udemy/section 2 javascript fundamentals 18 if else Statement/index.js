ba676c715534eda25a6258baa645cc84404ad885
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-18 16:44:02 +0530

    if else statement

---
+++ Udemy/section 2 javascript fundamentals 18 if else Statement/index.js
@@ -0,0 +1,14 @@
+const age = 18;
+const driverName = { Name: "yogesh", age: 19 };
+
+if (driverName.age <= 18) {
+  console.log(`Hello Mr ${driverName.Name} You Are Aligible To Drive`);
+} else {
+  console.log(`Hello Mr ${driverName.Name} You Are Not Aligible To Drive`);
+}
+
+if (driverName.age >= 18) {
+  console.log(`Hello Mr ${driverName.Name} You Are Aligible To Drive`);
+} else {
+  console.log(`Hello Mr ${driverName.Name} You Are Not Aligible To Drive`);
+}
