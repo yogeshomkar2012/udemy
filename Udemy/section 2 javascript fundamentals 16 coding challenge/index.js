6822550d3ef5246ebba5aab5233d113e18b28300
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-18 16:07:33 +0530

    challenge 16

---
+++ Udemy/section 2 javascript fundamentals 16 coding challenge/index.js
@@ -0,0 +1,40 @@
+/*
+CHALLENGE #1
+Mark and John are trying to compare their BMI 
+(Body Mass Index), which is calculated using the 
+formula: BMI = mass / (height * height) 
+(mass in kg and height in meters).
+
+Your task is to write some code to help them:
+
+1.Store Mark's and John's mass and height in 
+variables called massMark, heightMark, 
+massJohn and heightJohn.
+
+2.Calculate both their BMIs using the formula, 
+and store the results in two variables called 
+BMIMark and BMIJohn.
+
+3.Log the value of BMIMark and 
+BMIJohn to the console.
+
+4.BONUS: Create a boolean variable markHigherBMI 
+containing information about whether Mark has a 
+higher BMI than John. Log it to the console too
+
+TEST DATA: Marks weighs 78 kg and is 1.69 m tall. 
+John weighs 92 kg and is 1.95 m tall.
+
+
+
+
+*/
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = 78 / (1.69 * 1.69);
+const BMIJohn = 92 / (1.95 * 1.95);
+console.log(BMIMark, BMIJohn);
