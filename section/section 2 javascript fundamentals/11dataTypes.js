1d381160d4f44547fb94370be9bb559ca6416651
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-18 12:59:48 +0530

    data types

---
+++ section/section 2 javascript fundamentals/11dataTypes.js
@@ -0,0 +1,13 @@
+//  the 7 primitive Data Types
+
+console.log(`
+1. Number : floating pointg Number let age = 23,
+2. String: Sequence of Charecters let name = "Yogesh",
+3. Boolean: Logical type That Can only be true or false let name = true,
+
+4. undefined : value taken by a variable that is not Yet Defined(empty Value),
+5. Null : Also Empty Value,
+6. Symbol(ES2015) : Value that is unique and cannot be changed,
+7. BigInt
+
+`);
