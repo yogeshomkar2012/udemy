6ca54c01f2407a8906201d227c7f8384db28baa9
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-18 22:05:36 +0530

    tada types

--- Udemy/Section 2/11 dataTypes/index.js
+++ Udemy/Section 2/11 dataTypes/index.js
@@ -2,12 +2,11 @@
 
 console.log(`
 1. Number : floating pointg Number let age = 23,
-2. String: Sequence of Charecters let name = "Yogesh",
-3. Boolean: Logical type That Can only be true or false let name = true,
+2. String : Sequence of Charecters let name = "Yogesh",
+3. Boolean : Logical type That Can only be true or false let name = true,
 
 4. undefined : value taken by a variable that is not Yet Defined(empty Value),
 5. Null : Also Empty Value,
 6. Symbol(ES2015) : Value that is unique and cannot be changed,
-7. BigInt
-
+7. BigInt 
 `);
