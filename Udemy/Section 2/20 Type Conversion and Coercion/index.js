b5e8b66568b99b2af578bbb3726bd04f623b074f
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-18 17:49:34 +0530

    Type Conversion and Coercion

---
+++ Udemy/Section 2/20 Type Conversion and Coercion/index.js
@@ -0,0 +1,17 @@
+// convertion string to Number
+const inputYear = "1984";
+
+console.log(Number(inputYear));
+/**  when get data from data Base must 
+covert number string to nmuber like above  */
+
+/**  Coercion is the automatic conversion of 
+values from one data type to another. */
+console.log( +inputYear)
+
+
+
+
+
+
+
