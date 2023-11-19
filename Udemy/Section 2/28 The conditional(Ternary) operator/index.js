88c2c9e83e1733e7a61e11c77f827a83d7421b9c
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-19 13:33:58 +0530

    The conditional(Ternary) operator

---
+++ Udemy/Section 2/28 The conditional(Ternary) operator/index.js
@@ -0,0 +1,16 @@
+
+const age = 17;
+
+// ifstatement
+if (age >= 18) {
+    console.log('can drink')
+}
+
+// ternary oprator 
+age >= 18 ? console.log("can drink") : console.log("should not drink ")
+
+
+
+
+
+
