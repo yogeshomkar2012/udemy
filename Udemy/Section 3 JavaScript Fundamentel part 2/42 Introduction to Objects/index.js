89cc2542059da95d79a92cd22c07c9a242792274
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-20 16:52:16 +0530

    Introduction to Objects

---
+++ Udemy/Section 3 JavaScript Fundamentel part 2/42 Introduction to Objects/index.js
@@ -0,0 +1,10 @@
+const yogesh = {
+  // key and values
+  fname: "yogesh",
+  ln: "hv",
+  birthyear: 1984,
+  age: 2023 - 1984,
+  friends: ["kamala", "kumara", "chandra"],
+  win: true,
+};
+console.log(yogesh);
