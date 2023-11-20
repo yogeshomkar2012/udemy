29caa3abc973a1ca3cf7cc86e884e18962790672
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-20 08:34:48 +0530

    Functions Calling Other Functions

---
+++ Udemy/Section 3 JavaScript Fundamentel part 2/36 Functions Calling Other Functions/index.js
@@ -0,0 +1,26 @@
+// Exmple One
+const fruitProccessor = (apples, oranges) => {
+  const applePieces = (fruits) => {
+    return fruits * 4;
+  };
+  const orangePieces = (fruits) => {
+    return fruits * 4;
+  };
+  const juice = `Juce with ${applePieces(
+    apples
+  )} pieces of apple and ${orangePieces(oranges)} pices of orange`;
+  return juice;
+};
+console.log(fruitProccessor(2, 3));
+
+// Example Two
+const cutPices = () => {
+  return fruits * 4;
+};
+
+const fruitProccessors = (apples, oranges) => {
+  const applePieces = cutPices(apples);
+  const orangePieces = cutPices(oranges);
+  return `Juce with ${applePieces} pieces of apple and ${orangePieces} pices of orange`;
+};
+console.log(fruitProccessors(2, 3));
