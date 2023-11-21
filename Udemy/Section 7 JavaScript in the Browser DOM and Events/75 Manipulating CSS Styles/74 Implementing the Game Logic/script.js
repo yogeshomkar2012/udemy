cfa7f776857207dc0b33b725f46e2b2b7e9e0565
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-21 16:52:09 +0530

    Manipulating CSS Styles

---
+++ Udemy/Section 7 JavaScript in the Browser DOM and Events/75 Manipulating CSS Styles/74 Implementing the Game Logic/script.js
@@ -0,0 +1,58 @@
+'use strict';
+/** 
+console.log(
+  `DOM: DOCUMENT OBJECT MODEL : structured represntation 
+    of the html documents. Allows javascript to access 
+    html elements and styles to manipulation them`
+);
+// selecting dom element and assigning to a variable
+// const message = document.querySelector('.message');
+// console.log(message);
+// targeting text content of selected element
+// console.log(message.textContent);
+// changing(manuplating) text of targeted element
+// console.log((message.textContent = 'text content changed'));
+
+console.log(document.querySelector(".message").textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector(".score").textContent = 16;
+document.querySelector(".guess").value = 16;
+console.log(document.querySelector(".guess").value = 16)
+*/
+const message = document.querySelector('.message');
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // no Value
+  if (!guess) {
+    message.textContent = 'Please Enter A Number';
+    // player is win
+  } else if (guess === secretNumber) {
+    message.textContent = 'Correct Number';
+    document.querySelector('body').style.backgroundColor = 'green';
+
+    // guess Too Heigh secrate Number
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      message.textContent = 'Too Heigh';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      message.textContent = 'You Loos the game';
+      document.queryS;
+      elector('.score').textContent = 0;
+    }
+    // guess Too Low secrate Number
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      message.textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      message.textContent = 'You Loos the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
