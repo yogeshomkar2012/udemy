e1852cbc27f2dc54cac92d2b13a7de7cdf7a14db
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-21 13:32:34 +0530

    What's the DOM and DOM

---
+++ Udemy/Section 7 JavaScript in the Browser DOM and Events/71 What's the DOM and DOM/script.js
@@ -0,0 +1,7 @@
+'use strict';
+console.log(document.querySelector('.message'));
+console.log(
+  `DOM: DOCUMENT OBJECT MODEL : structured represntation 
+    of the html documents. Allows javascript to access 
+    html elements and styles to manipulation them`
+);
