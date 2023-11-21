dfd63928508dde31b348075a320bb43be76e3439
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-21 09:54:32 +0530

    The while Loop

---
+++ Udemy/Section 3 JavaScript Fundamentel part 2/49 The while Loop/index.js
@@ -0,0 +1,14 @@
+console.log("------- for loop-------")
+
+for (let rep = 1; rep <= 5; rep++) {
+  console.log(`from for loop lifting weights repetition ${rep}`);
+}
+
+
+console.log("------- While loop-------")
+
+let rep = 1;
+while(rep <= 5){
+    console.log(`from while loop lifting weights repetition ${rep}`);
+  rep++
+}
