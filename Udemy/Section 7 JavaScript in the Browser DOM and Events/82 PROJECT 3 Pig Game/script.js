cf9c8d936fd0fe63e5ff2998d9ba86de03612bdc
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-22 15:35:26 +0530

    PROJECT 3 Pig Game

---
+++ Udemy/Section 7 JavaScript in the Browser DOM and Events/82 PROJECT 3 Pig Game/script.js
@@ -0,0 +1,10 @@
+'use strict';
+// selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+//starting Condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden')
