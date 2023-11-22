c839273769a56897f5d697d300ee19eaf19c9a0a
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-22 15:58:41 +0530

    Rolling the Dice

---
+++ Udemy/Section 7 JavaScript in the Browser DOM and Events/83 Rolling the Dice/script.js
@@ -0,0 +1,22 @@
+'use strict';
+// selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//starting Condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const eventList = {
+  click: 'click',
+};
+
+//Rolling dice funtionality
+btnRoll.addEventListener(eventList.click, function () {
+  alert();
+});
