174ecaf9254448a6c062cf3decea3306fe02d0b3
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-22 16:39:29 +0530

    Rolling the Dice

--- Udemy/Section 7 JavaScript in the Browser DOM and Events/83 Rolling the Dice/script.js
+++ Udemy/Section 7 JavaScript in the Browser DOM and Events/83 Rolling the Dice/script.js
@@ -2,6 +2,8 @@
 // selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,11 +14,22 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const eventList = {
-  click: 'click',
-};
-
 //Rolling dice funtionality
-btnRoll.addEventListener(eventList.click, function () {
-  alert();
+let currentScore = 0;
+console.log(currentScore);
+btnRoll.addEventListener('click', function () {
+  // generating random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // check for rolled 1 if true switch next player
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // switch next player
+  }
 });
