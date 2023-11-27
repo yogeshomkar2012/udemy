427f75162a9365b7802c5442265c09d69359e517
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-27 15:33:45 +0530

    CodingChalange-3

--- Udemy/section 9 Data-Structures-Modern-Operators-and-string/120CodingChalange-3.js
+++ Udemy/section 9 Data-Structures-Modern-Operators-and-string/120CodingChalange-3.js
@@ -14,24 +14,7 @@ const gameEvents = new Map([
   [92, "🔶 Yellow card"],
 ]);
 
-console.log(`Coding Challenge #3`);
-console.log(`Let's continue with our football betting app! This time,
-we have a map with a log of the events that happened 
-during the game. The values are the events themselves, 
-and the keys are the minutes in which each event 
-happened (a football game has 90 minutes plus some extra 
-   time).`);
-// task 1
-console.log(`task 1 : \n Create an array 'events' of the 
- different game events that happened (no duplicates)`);
-const events = [...new Set(gameEvents.values())];
-console.log(events);
-//task 2
-console.log(`task 2 : \n After the game has finished, 
- is was found that the yellow card from minute 64 was
- unfair. So remove this event from the game events log.`);
-console.log(gameEvents.delete(64));
-console.log(gameEvents);
+
 // task 3
 console.log(`task 3 : \n Print the following string to the 
 console: "An event happened, on average, every 9 minutes"
