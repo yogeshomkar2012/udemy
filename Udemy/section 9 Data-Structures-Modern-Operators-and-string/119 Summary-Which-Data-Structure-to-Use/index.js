bd0405c9cb0dda22852072b4ca6d34738f8202f3
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2023-11-27 15:05:50 +0530

    Summary-Which-Data-Structure-to-Use

---
+++ Udemy/section 9 Data-Structures-Modern-Operators-and-string/119 Summary-Which-Data-Structure-to-Use/index.js
@@ -0,0 +1,16 @@
+console.log(`
+1). from the program itsele : status message,
+2). From the Ui : from user,
+3). from External Sources : web API
+
+data structure
+A. Simple List // simple list
+Array or Sets
+B. Objects or Maps // key pairs
+
+
+
+`);
+console.log(`convert object To Array`);
+const conObjToArr = Object.entries(openingHours);
+console.log(conObjToArr);
