8a2a60acb5d7a34160c116cbf7a75be927d3466f
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2024-02-22 13:15:53 +0530

    91_Working_with_EJS

--- UdemyNodeJs/section_6_working_with_Dynamic_Content_and_AddingTmplate/91_Working_with_EJS/routes/shop.js
+++ UdemyNodeJs/section_6_working_with_Dynamic_Content_and_AddingTmplate/91_Working_with_EJS/routes/shop.js
@@ -8,11 +8,14 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop');
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
-// router.get('/', (req, res, next) => {
-//   console.log(adminData.products)
-//   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-// });
 
 module.exports = router;
