dff9302573d8bda4e212794445b4d5ec092cae6d
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2024-02-22 17:48:12 +0530

    94_assignment_solution

---
+++ UdemyNodeJs/section_6_working_with_Dynamic_Content_and_AddingTmplate/94_assignment_solution/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: false }));
+const users = [];
+app.get('/', (req, res, next) => {
+  res.render('index', { pageTitle: 'add user' });
+});
+app.post('/add-user', (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect('/users');
+});
+app.get('/users', (req, res, next) => {
+  res.render('users', {
+    pageTitle: 'user List',
+    users: users,
+  });
+});
+app.listen(3000);
