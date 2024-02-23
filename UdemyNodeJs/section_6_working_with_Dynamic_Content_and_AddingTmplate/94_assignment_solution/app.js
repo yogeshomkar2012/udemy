b20d4c9c04b792ede1d0e0a7ff572c618a186f91
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2024-02-23 15:00:30 +0530

    98_Adding_Controllers

--- UdemyNodeJs/section_6_working_with_Dynamic_Content_and_AddingTmplate/94_assignment_solution/app.js
+++ UdemyNodeJs/section_6_working_with_Dynamic_Content_and_AddingTmplate/94_assignment_solution/app.js
@@ -5,7 +5,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
+
 const users = [];
+
 app.get('/', (req, res, next) => {
   res.render('index', { pageTitle: 'add user' });
 });
