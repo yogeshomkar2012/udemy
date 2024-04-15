3ea451fad542430f334d005675f2c2d14e712284
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2024-04-15 15:18:08 +0530

    63_Handling_Different_Routes

--- UdemyNodeJs/section_5_working_with_express_js/61_how_middleware_works/app.js
+++ UdemyNodeJs/section_5_working_with_express_js/61_how_middleware_works/app.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
@@ -11,6 +10,4 @@ app.use((req, res, next) => {
   res.send('<h1>middle ware</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(4000);
+app.listen(4000);
--- UdemyNodeJs/section_5_working_with_express_js/63_Handling_Different_Routes/app.js
+++ UdemyNodeJs/section_5_working_with_express_js/63_Handling_Different_Routes/app.js
@@ -1,11 +1,24 @@
+// const express = require('express');
+// const app = express();
+// app.use('/add-product', (req, res, next) => {
+//   console.log('in middleware');
+//   res.send('<h1>hell0 add product express</h1>');
+// });
+// app.use('/', (req, res, next) => {
+//   console.log('in middleware');
+//   res.send('<h1>hell from express</h1>');
+// });
+// app.listen(2000);
+
 const express = require('express');
 const app = express();
-app.use('/add-product', (req, res, next) => {
-  console.log('in middleware');
-  res.send('<h1>hell0 add product express</h1>');
+
+app.use('/add-products', (req, res, next) => {
+  res.send('<h1>Add Products</h1>');
 });
+
 app.use('/', (req, res, next) => {
-  console.log('in middleware');
-  res.send('<h1>hell from express</h1>');
+  res.send('<h3>Home page</h3>');
 });
-app.listen(2000);
+
+app.listen(4000);
