f67d05b6a3d1c655f3e6f397b6da21f530be9b21
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2024-04-15 16:00:44 +0530

    64_parsing_incoming_request

--- UdemyNodeJs/section_5_working_with_express_js/64_parsing_incoming_request/app.js
+++ UdemyNodeJs/section_5_working_with_express_js/64_parsing_incoming_request/app.js
@@ -1,11 +1,34 @@
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use('/add-product', (req, res, next) => {
+//   res.send(
+//     "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+//   );
+// });
+// app.use('/product', (req, res, next) => {
+//   console.log(req.body);
+//   res.redirect('/');
+// });
+// app.use('/', (req, res, next) => {
+//   res.send('<h1>hello from express!</h1>');
+// });
+
+// app.listen(2000);
+////////////////// or ////////////////////////
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
 app.use('/add-product', (req, res, next) => {
   res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+    '<form action="/product" method="POST"><input type="text" name="title"/> <button type="submit">Add Product</button></form>'
   );
 });
 app.use('/product', (req, res, next) => {
@@ -13,7 +36,7 @@ app.use('/product', (req, res, next) => {
   res.redirect('/');
 });
 app.use('/', (req, res, next) => {
-  res.send('<h1>hello from express!</h1>');
+  res.send('<h1>Hello From Express js</h1>');
 });
 
-app.listen(2000);
+app.listen(4000);
