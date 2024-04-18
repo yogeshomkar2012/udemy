40735a76ed07c0c7112343bb5f90a2a3d4f6a15d
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2024-04-18 16:34:02 +0530

    98_adding_controllers

--- UdemyNodeJs/section_7_the_model_view_controller/98_adding_controllers/app.js
+++ UdemyNodeJs/section_7_the_model_view_controller/98_adding_controllers/app.js
@@ -1,19 +1,21 @@
 const path = require('path');
-
+const rootDir = require('./util/path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- UdemyNodeJs/section_7_the_model_view_controller/Exersice/routes/admin.js
+++ UdemyNodeJs/section_7_the_model_view_controller/Exersice/routes/admin.js
@@ -13,9 +13,9 @@ router.get('/add-product', (req, res, next) => {
   res.render('add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    // formsCSS: true,
+    // productCSS: true,
+    // activeAddProduct: true
   });
 });
 
