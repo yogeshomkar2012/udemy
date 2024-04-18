d16932cd3f8255423996220a371a9c78526828e1
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2024-04-18 16:56:59 +0530

    99_finishing_controllers

---
+++ UdemyNodeJs/section_7_the_model_view_controller/99_finishing_controllers/app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const rootDir = require('./util/path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(rootDir, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
