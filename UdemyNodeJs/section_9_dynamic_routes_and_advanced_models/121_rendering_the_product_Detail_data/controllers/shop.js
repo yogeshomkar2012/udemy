3e1f0d6d7e77db99003c4b6e160783b6e7ba559a
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2024-05-10 12:15:42 +0530

    120_loading_product_detail_data

--- UdemyNodeJs/section_9_dynamic_routes_and_advanced_models/121_rendering_the_product_Detail_data/controllers/shop.js
+++ UdemyNodeJs/section_9_dynamic_routes_and_advanced_models/121_rendering_the_product_Detail_data/controllers/shop.js
@@ -5,7 +5,7 @@ exports.getProducts = (req, res, next) => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
-      path: '/products',
+      path: '/products'
     });
   });
 };
@@ -14,12 +14,9 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product:product,
-      pageTitle: product.title,
-      path:'/products'
-    });
+    console.log(product);
   });
+  res.redirect('/');
 };
 
 exports.getIndex = (req, res, next) => {
@@ -27,7 +24,7 @@ exports.getIndex = (req, res, next) => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
-      path: '/',
+      path: '/'
     });
   });
 };
@@ -35,20 +32,20 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
-    pageTitle: 'Your Cart',
+    pageTitle: 'Your Cart'
   });
 };
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
-    pageTitle: 'Your Orders',
+    pageTitle: 'Your Orders'
   });
 };
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
-    pageTitle: 'Checkout',
+    pageTitle: 'Checkout'
   });
 };
