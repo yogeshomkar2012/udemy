cf95704f35e446abcb04650e76414174a3ec1a8f
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2024-05-22 13:49:36 +0530

    182_finishing_the_database

---
+++ UdemyNodeJs/section_12_working_with_nosql_and_useng_mangodb/182_finishing_the_database/models/product.js
@@ -0,0 +1,34 @@
+const getdb = require('../util/database').getdb;
+class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+  save() {}
+}
+
+// const Product = sequelize.define('product', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true,
+//   },
+//   title: Sequelize.STRING,
+//   price: {
+//     type: Sequelize.DOUBLE,
+//     allowNull: false,
+//   },
+//   imageUrl: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   description: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+// });
+
+module.exports = Product;
