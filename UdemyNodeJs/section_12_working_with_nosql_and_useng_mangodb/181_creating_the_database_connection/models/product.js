771a5bb999744786d05a770d62ff93613bec3ad5
yogesh <75164383+yogeshomkar2012@users.noreply.github.com>
2024-05-22 13:19:59 +0530

    181_creating_the_database_connection

---
+++ UdemyNodeJs/section_12_working_with_nosql_and_useng_mangodb/181_creating_the_database_connection/models/product.js
@@ -0,0 +1,33 @@
+class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+  save() {}
+}
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
